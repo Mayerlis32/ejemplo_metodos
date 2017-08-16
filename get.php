9ee51883c2166b2324e20c898b4b21622c32476a
Mayerlis32 <30443639+Mayerlis32@users.noreply.github.com>
2017-08-16 12:47:08 -0500

    Add files via upload

---
+++ get.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form action="procesar-datos.php" method="get">
+      Nombre de usuario:
+      <br>
+      <input type="text" name="usuario">
+      <br>
+      Contraseña:
+      <br>
+      <input type="password" name="clave">
+      <br><br>
+      <input type="submit" value="Enviar datos">
+</form>
+
+  </body>
+</html>
